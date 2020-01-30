a3cbe81ab3d5d5be4d806dbdd43d0cc146dcf62b
slavious <slavious@gmail.com>
2020-01-30 15:02:58 +0200

    initial

---
+++ src/Controller/BaseController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Routing\Annotation\Route;
+
+class BaseController extends AbstractController
+{
+    /**
+     * @Route("/base", name="base")
+     */
+    public function index(Request $request)
+    {
+        if ($request->get('run')) {
+            system('c:\WINDOWS\system32\cmd.exe /c START C:\Windows\System32\drivers\etc\startvr.bat');
+            return json_encode(["success" == true]);
+        }
+        return $this->render('base/index.html.twig', [
+            'peremennaya_1' => 'Kostya pizdyuk!',
+            'peremennaya_2' => 'Uchitsa rabotat` s symfony',
+
+        ]);
+    }
+}
---
+++ src/Entity/Building.php
@@ -0,0 +1,108 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass="App\Repository\BuildingRepository")
+ */
+class Building
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $name;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $url;
+
+    /**
+     * @ORM\Column(type="string", length=255, nullable=true)
+     */
+    private $logo;
+
+    /**
+     * @ORM\Column(type="string", length=255, nullable=true)
+     */
+    private $background;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $brand_name;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): self
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getUrl(): ?string
+    {
+        return $this->url;
+    }
+
+    public function setUrl(string $url): self
+    {
+        $this->url = $url;
+
+        return $this;
+    }
+
+    public function getLogo(): ?string
+    {
+        return $this->logo;
+    }
+
+    public function setLogo(?string $logo): self
+    {
+        $this->logo = $logo;
+
+        return $this;
+    }
+
+    public function getBackground(): ?string
+    {
+        return $this->background;
+    }
+
+    public function setBackground(?string $background): self
+    {
+        $this->background = $background;
+
+        return $this;
+    }
+
+    public function getBrandName(): ?string
+    {
+        return $this->brand_name;
+    }
+
+    public function setBrandName(string $brand_name): self
+    {
+        $this->brand_name = $brand_name;
+
+        return $this;
+    }
+}
