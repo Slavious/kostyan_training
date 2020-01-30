1ceaf7829cf955a47942aded5f3508b919efffc9
Konstantin <whitecat.dev@gmail.com>
2020-01-30 17:25:37 +0200

    fix

--- src/Controller/AdminController.php
+++ src/Controller/AdminController.php
@@ -9,6 +9,14 @@ use App\Entity\Building;
 class AdminController extends AbstractController
 {
 
+    /**
+     * @Route("/admin", name="admin")
+     */
+    public function index(Request $request){return $this->render('admin/building_add.html.twig', [
+        'peremennaya_1' => 'Kostya',
+        'peremennaya_2' => 'Uchitsa rabotat` s symfony',
+
+    ]);}
 
     /**
      * @Route("/admin/building/add", name="buildinAdd")
