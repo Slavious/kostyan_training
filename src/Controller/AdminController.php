4722fc1d6850d9e2be15d1b6dd5f84de3c69dea6
slavious <slavious@gmail.com>
2020-02-03 14:10:43 +0200

    1

--- src/Controller/AdminController.php
+++ src/Controller/AdminController.php
@@ -10,15 +10,6 @@ class AdminController extends AbstractController
 {
 
     /**
-     * @Route("/admin", name="admin")
-     */
-    public function index(Request $request){return $this->render('admin/building_add.html.twig', [
-        'peremennaya_1' => 'Kostya',
-        'peremennaya_2' => 'Uchitsa rabotat` s symfony',
-
-    ]);}
-
-    /**
      * @Route("/admin/building/add", name="buildinAdd")
      */
     public function buildingAdd()
